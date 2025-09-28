017d592845ddcfabdd6272dc92d9494c62500a52
multipleof4 <planetrenox@protonmail.com>
2025-09-28 11:18:49 -0700

    Feat: Add catch-all redirect handler for slugs

---
+++ functions/[slug].js
@@ -0,0 +1,9 @@
+export async function onRequestGet({ request, params, env }) {
+  try {
+    const dest = await env.KV_EV.get(params.slug);
+    const url = dest ? `https://${dest}` : new URL('/', request.url).href;
+    return Response.redirect(url, dest ? 301 : 302);
+  } catch (e) {
+    return new Response(e.message, { status: 500 });
+  }
+}
