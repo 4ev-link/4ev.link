e4fac64771a52775cdf4f8703632c7d01dc89347
multipleof4 <planetrenox@protonmail.com>
2025-10-11 08:56:11 -0700

    Feat: Return analytics status along with destination

--- functions/api/links/get.js
+++ functions/api/links/get.js
@@ -7,7 +7,10 @@ export async function onRequestGet({ request, env }) {
         const dest = await env.KV_EV.get(slug);
         if (!dest) return new Response("Link not found", { status: 404 });
 
-        return Response.json({ destination_url: dest });
+        const analytics_enabled = dest.startsWith('✺');
+        const destination_url = analytics_enabled ? dest.substring(1) : dest;
+
+        return Response.json({ destination_url, analytics_enabled });
     } catch (e) {
         return new Response(e.message, { status: 500 });
     }
